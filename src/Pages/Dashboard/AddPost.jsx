744038ffc866d563fd129cf575e64cf47bbf9289
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-24 17:00:11 +0600

    add post page is add

--- src/Pages/Dashboard/AddPost.jsx
+++ src/Pages/Dashboard/AddPost.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../Shared/SectionTitle";
 
 const AddPost = () => {
-  return <div>add post</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="bg-slate-300 p-8 mx-auto">
+      <SectionTitle
+        title={"Add Post"}
+        subtitle={"Add Your Post"}
+      ></SectionTitle>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-bold">Post Title</span>
+            </label>
+            <input
+              {...register("title", { required: true })}
+              type="text"
+              placeholder="Post Title"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="flex space-x-5">
+            <div className="w-1/2">
+              <label className="label">
+                <span className="label-text font-bold">UpVote</span>
+              </label>
+              <input
+                {...register("upVote", { required: true })}
+                type="text"
+                placeholder="0"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="label">
+                <span className="label-text font-bold">DownVote</span>
+              </label>
+              <input
+                {...register("downVote", { required: true })}
+                type="text"
+                placeholder="0"
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+          <div className="w-1/2">
+            <label className="label">
+              <span className="label-text">Select Your Tag</span>
+            </label>
+            <select
+              defaultValue={"default"}
+              className="select select-primary w-full"
+              {...register("tag")}
+            >
+              <option disabled value="default">
+                Select Your Tag
+              </option>
+              <option value="HTML">HTML</option>
+              <option value="ReactJS">ReactJS</option>
+              <option value="Java">Java</option>
+              <option value="Python">Python</option>
+              <option value="CSS">CSS</option>
+              <option value="NodeJS">NodeJS</option>
+              <option value="Django">Django</option>
+              <option value="MongoDB">MongoDB</option>
+              <option value="SQLite">JavaScSQLiteript</option>
+            </select>
+          </div>
+          <div className="w-full mt-3">
+            <label className="label">
+              <span className="label-text font-bold">DownVote</span>
+            </label>
+            <textarea
+              {...register("description")}
+              placeholder="Post Description"
+              className="textarea textarea-bordered textarea-lg w-full"
+            ></textarea>
+          </div>
+        </div>
+
+        <input
+          className="btn btn-outline btn-success items-center text-center mx-auto flex justify-center"
+          type="submit"
+          value="Add Post"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default AddPost;
--- src/Pages/Dashboard/MyPosts.jsx
+++ src/Pages/Dashboard/MyPosts.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { AiOutlineLike } from "react-icons/ai";
 
 const MyPosts = () => {
-  return <div>my post</div>;
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">What is React?</h2>
+          <p>Vote : 41</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyPosts;
